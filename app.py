2836da931a1ad5502e7dad3da71a20acb1d4ac0b
hawuol <hawuol@guro.sen.hs.kr>
2026-04-03 08:07:42 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import streamlit as st
 
-st.header(" ~~AI 챗봇~~ 만들기")
+st.markdown("# AI 챗봇 만들기")
 st.markdown("---")
 st.markdown("## 질문을 하시면 AI 친구가 응답합니다.")
-st.markdown("1. **제작 언어**: `Python`")
-st.markdown("2. **프레임워크**: `Streamlit`")
+st.markdown("* **제작 언어**: `Python`")
+st.markdown("* **프레임워크**: `Streamlit`")
 st.markdown("### `최근 대화`")
 st.markdown("~~질문을 자유롭게 입력하세요.~~")
