58e9924056ea787f9ae340e0d4dc26f426d23b1f
hawuol <hawuol@guro.sen.hs.kr>
2026-04-02 15:43:45 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
 st.markdown("AI 챗봇 만들기")
-st.markdown("#오늘의 질문")
-st.markdown("##입력 안내")
-st.markdown("###질문을 자유롭게 입력하세요.")
+st.markdown("# 오늘의 질문")
+st.markdown("## 입력 안내")
+st.markdown("### 질문을 자유롭게 입력하세요.")
