5194951bb9d459f30d148941ef62c231d99544ef
hawuol <hawuol@guro.sen.hs.kr>
2026-04-09 14:05:44 +0900

    Update app.py

--- app.py
+++ app.py
@@ -3,5 +3,5 @@ import streamlit as st
 st.title("카운터 앱")
 st.session_state.count = 0
 if st.button("증가"):
-    count = count+1
-st.markdown(f"## 현재 숫자: `{count}`")
+    st.session_state.count += 1
+st.markdown(f"## 현재 숫자: `{st.session_state.count}`")
