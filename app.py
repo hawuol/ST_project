cef33254c8e1ce3635fa25b0c874944cec92958d
hawuol <hawuol@guro.sen.hs.kr>
2026-04-21 12:24:53 +0900

    Update app.py

--- app.py
+++ app.py
@@ -79,15 +79,29 @@ def page_report():
 
 def page_ai_coach():
     st.header("🧐 AI 코치와 대화하기")
-    prompt = st.chat_input("질문을 입력하세요")
-    if prompt:
-        st.write(prompt)
-    if st.button("보내기"):
-        response = ai_client.responses.create(
-            model="gpt-5.4-mini",
-            input=prompt
-        )
-        st.write(response.output_text)
+    if "messages" not in st.session_state:
+        st.session_state.messages = [
+            {"role": "system", "content": "너는 사용자의 할 일 목록과 달성 정도를 분석하여 조언하는 열정적인 코치야. 사용자가 더 멋진 삶을 살 수 있도록 명확한 조언과 응원해줘."}
+        ]
+    for message in st.session_state.messages:
+        if message["role"] != "system":
+            with st.chat_message(message["role"]):
+                st.markdown(message["content"])
+                
+    question = st.chat_input("질문을 입력하세요")
+    if question:
+        st.session_state.messages.append({"role": "user", "content": question})
+        with st.chat_message("user"):
+            st.markdown(question)
+        with st.chat_message("ai"):
+            prompt = st.session_state.messages
+            with st.spinner("코치가 생각 중..."):
+                response = ai_client.chat.completions.create(
+                    model="gpt-5.4-mini",
+                    messages=prompt)
+                full_response = response.choices[0].message.content
+                st.markdown(full_response)
+        st.session_state.messages.append({"role": "ai", "content": full_response})
 
 pg = st.navigation([
     st.Page(page_motto, title="오늘의 다짐", icon="📣"),
